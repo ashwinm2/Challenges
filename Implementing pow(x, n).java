4112c7c9491d3813d7049aab8383b772aceede5d
ashwinm2 <ashwinspocket@gmail.com>
2017-05-15 12:24:33 -0700

    Challenges

---
+++ Implementing pow(x, n).java
@@ -0,0 +1,20 @@
+# Implementing pow(x, n)
+public class Solution {
+    public double myPow(double x, int n) {
+        double temp=x;
+        int m = n;
+        if(m==0)
+            return 1;
+        temp = myPow(x,m/2);
+        
+        if(m%2==0)
+            return temp*temp;
+        else 
+        {
+        if(m > 0)
+            return x*temp*temp;
+        else
+            return (temp*temp)/x;
+        }
+    }
+}
